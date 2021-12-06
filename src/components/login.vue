88141b8b5cabfb04cc23ff7f63bc5fc4ccb0f3c5
plusone <zhuozhengyi@icloud.com>
2021-12-06 18:54:09 +0800

    Finish Login Update

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,211 +4,158 @@
 -->
 
 <template>
-    <el-container style="height: 100%;">
-        <el-header style="height: auto;">
-            <!--顾客身份-->
-            <el-image 
-                :src="customerIcon"
-                style="width: 10%;position:absolute;top:20%;cursor: pointer;"
-                @click="changeToCustomer(1)"
-                @mouseover="changeToCustomer(2)" 
-                @mouseout="changeToCustomer(3)"
-            ></el-image>
-            <p 
-            style="font-size: xx-large;
+  <el-container style="height: 100%;">
+    <el-header style="height: auto;">
+      <!--顾客身份-->
+      <el-image
+          :src="customerIcon"
+          style="width: 10%;position:absolute;top:20%;cursor: pointer;"
+          @click="changeToCustomer(1)"
+          @mouseover="changeToCustomer(2)"
+          @mouseout="changeToCustomer(3)"
+      ></el-image>
+      <p
+          style="font-size: xx-large;
             margin-top: 10px;
             text-align: center;">
-            登录</p>
-            <!--房东身份-->
-            <el-image 
-                :src="hostIcon"
-                style="width: 10%;position:absolute;top:20%;right: 10%;cursor: pointer;"
-                @click="changeToHost(1)"
-                @mouseover="changeToHost(2)" 
-                @mouseout="changeToHost(3)"
-            ></el-image>
-            <p>登录“归宿”，体验专属于你的精彩世界！</p>
-        </el-header>
-        <el-form style="margin-top: 20px;margin-left: 40px; margin-right: 40px;height: 100%;">
-            <el-row style="height: 100%;">
-                <el-form-item>
-                    <el-input v-model="phonenumber" placeholder="手机号"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-input 
-                    v-model="password" 
-                    placeholder="请输入密码" 
-                    show-password
-                    ></el-input>
-                </el-form-item>
-                <!--验证码-->
-                <el-form-item style="height: 100%;margin-bottom: 0;">
-                    <el-col :span="11">
-                        <el-input 
-                        v-model="verifycode" 
-                        placeholder="验证码"
-                        maxlength=4
-                        >
-                        </el-input>
-                    </el-col>
-                    <el-col :span="11" style="margin-left: 30px;height: 100%;">
-                        <el-image
-                        :src="codeimg"
-                        @click="updateVerifyCode"
-                        style="width: 140px;height:50px;cursor: pointer;">
-                        </el-image>
-                    </el-col>
-                    
-                </el-form-item>
-                <el-form-item style="margin-bottom: 10px;">
-                    <el-col :span="11" style="margin-left: 20px;">
-                        <el-checkbox 
-                        label="记住我" 
-                        name="type"
-                        v-model="rememberMe"
-                        ></el-checkbox>
-                    </el-col>
-                    <el-col :span="11" style="width: auto;margin-left: 40px;">
-                        <el-button 
-                        type="text"
-                        @click="forgetPassword"
-                        >
-                            忘记密码？</el-button>
-                    </el-col>
-                </el-form-item>
-                
-            </el-row>
-        </el-form>
-    </el-container>
+        登录</p>
+      <!--房东身份-->
+      <el-image
+          :src="hostIcon"
+          style="width: 10%;position:absolute;top:20%;right: 10%;cursor: pointer;"
+          @click="changeToHost(1)"
+          @mouseover="changeToHost(2)"
+          @mouseout="changeToHost(3)"
+      ></el-image>
+      <p>登录“归宿”，体验专属于你的精彩世界！</p>
+    </el-header>
+    <el-form style="margin-top: 20px;margin-left: 40px; margin-right: 40px;height: 100%;">
+      <el-row style="height: 100%;">
+        <el-form-item>
+          <el-input v-model="phonenumber" placeholder="手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+              v-model="password"
+              placeholder="请输入密码"
+              show-password
+          ></el-input>
+        </el-form-item>
+        <!--验证码-->
+        <el-form-item style="height: 100%;margin-bottom: 0;">
+          <el-col :span="11">
+            <el-input
+                v-model="verifycode"
+                placeholder="验证码"
+                maxlength=4
+            >
+            </el-input>
+          </el-col>
+          <el-col :span="11" style="margin-left: 30px;height: 100%;">
+            <el-image
+                :src="codeimg"
+                @click="updateVerifyCode"
+                style="width: 140px;height:50px;cursor: pointer;">
+            </el-image>
+          </el-col>
+
+        </el-form-item>
+        <el-form-item style="margin-bottom: 10px;">
+          <el-col :span="11" style="margin-left: 20px;">
+            <el-checkbox
+                label="记住我"
+                name="type"
+                v-model="rememberMe"
+            ></el-checkbox>
+          </el-col>
+          <el-col :span="11" style="width: auto;margin-left: 40px;">
+            <el-button
+                type="text"
+                @click="forgetPassword"
+            >
+              忘记密码？
+            </el-button>
+          </el-col>
+        </el-form-item>
+
+      </el-row>
+    </el-form>
+  </el-container>
 </template>
-  
+
 <script>
-import { getVerifyCode } from '@/api/public'
-import { mapMutations } from 'vuex';
+import {getVerifyCode} from '@/api/public'
+import {mapMutations} from 'vuex';
+
 export default {
-    name: 'LoginName',//这个LoginName最好和引入的vue的LoginName相同
-    data(){
-        return{
-            phonenumber:'',
-            password:'',
-            verifycode:'',
-            codeimg:'',
-            trueVerifycode:'',//正确的验证码
-            customerLogin:true,//标记当前是顾客登录还是房东登录
-            customerIcon:"https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIconSelected.jpg",
-            hostIcon:"https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIcon.jpg",
-            rememberMe:false,
-        }
-    },
-    created(){
-        /*
-        页面生成时更新
-        */
-
-       //判断是否有"记住我"信息
-       let rememberState = localStorage.getItem('rememberPhone');
-        
-       console.log('记住我的状态为：',rememberState);
-        if (rememberState === null || rememberState === '') {
-            console.log('上次操作没有选择记住我')
-        }
-        else{
-            console.log('上次操作选择了"记住我"，已自动读取')
-            this.phonenumber=localStorage.getItem('rememberPhone');
-            console.log('电话为:',this.phonenumber,localStorage.getItem('rememberPhone'));
-            this.password=localStorage.getItem('rememberPassword');
-            this.rememberMe=true;
-        }
-        this.updateVerifyCode();
-    },
-    methods:{
-        submitForm(){
-            console.log('点击登录键')
-            // this.$parent.$parent.$parent.$parent.$parent.dialogTableVisible=false;
-            this.$emit("logins")
-        },
-        updateVerifyCode(){
-            /*
-            更新验证码
-            */
-            console.log('正在尝试更新验证码');
-            getVerifyCode().then(response=>{
-                console.log('verifycode:',response);
-                this.codeimg=response.data.codeimg;
-                this.trueVerifycode=response.data.verifycode;
-            })
-        },
-        changeToCustomer(index){
-            if(index==1){
-                if(this.customerLogin){
-                    return;
-                }
-                this.$message({
-                    message: '已切换为顾客登录',
-                    type: 'success'
-                });
-                console.log('切换为顾客登录');
-                this.customerLogin=true;
-                this.customerIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIconSelected.jpg";
-                this.hostIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIcon.jpg";
-            }
-            else if (index==2&&!this.customerLogin){
-                //鼠标移动上来
-                this.customerIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIconHover.jpg";
-            }
-            else if (index==3){
-                //鼠标移动出去
-                if(this.customerLogin){
-                    this.customerIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIconSelected.jpg";
-                }
-                else{
-                    this.customerIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIcon.jpg";
-                }
-            }
-            
-        },
-        changeToHost(index){
-            if(index==1){
-                if(!this.customerLogin){
-                    return;
-                }
-                this.$message({
-                    message: '已切换为房东登录',
-                    type: 'success'
-                });
-                console.log('切换为房东登录');
-                this.customerLogin=false;
-                this.customerIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIcon.jpg";
-                this.hostIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIconSelected.jpg";
-            }
-            else if (index==2&&this.customerLogin){
-                //鼠标移动上来
-                this.hostIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIconHover.jpg";
-            }
-            else if (index==3){
-                //鼠标移动出去
-                if(!this.customerLogin){
-                    this.hostIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIconSelected.jpg";
-                }
-                else{
-                    this.hostIcon="https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIcon.jpg";
-                }
-            }
-
-        },
-        forgetPassword(){
-            /*
-            忘记密码
-            */
-            console.log('忘记密码按钮被触发')
-            let that=this
-            this.$router.push({path:'/forgetPassword',query:{
-                isCustomer:that.customerLogin
-            }});
-            //关闭登录界面
-            this.$emit('closeLogin');
-        }
+  name: 'LoginName',//这个LoginName最好和引入的vue的LoginName相同
+  data() {
+    return {
+      phonenumber: '',
+      password: '',
+      verifycode: '',
+      codeimg: '',
+      trueVerifycode: '',//正确的验证码
+      customerIcon: "https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/customerIconSelected.jpg",
+      hostIcon: "https://oliver-img.oss-cn-shanghai.aliyuncs.com/img/hostIconSelected.jpg",
+      rememberMe: false,
     }
+  },
+  created() {
+    /*
+    页面生成时更新
+    */
+
+    //判断是否有"记住我"信息
+    let rememberState = localStorage.getItem('rememberPhone');
+
+    console.log('记住我的状态为：', rememberState);
+    if (rememberState === null || rememberState === '') {
+      console.log('上次操作没有选择记住我')
+    } else {
+      console.log('上次操作选择了"记住我"，已自动读取')
+      this.phonenumber = localStorage.getItem('rememberPhone');
+      console.log('电话为:', this.phonenumber, localStorage.getItem('rememberPhone'));
+      this.password = localStorage.getItem('rememberPassword');
+      this.rememberMe = true;
+    }
+    this.updateVerifyCode();
+  },
+  methods: {
+    submitForm() {
+      console.log('点击登录键')
+      // this.$parent.$parent.$parent.$parent.$parent.dialogTableVisible=false;
+      this.$emit("logins")
+    },
+
+    updateVerifyCode() {
+      /*
+      更新验证码
+      */
+      console.log('正在尝试更新验证码');
+      getVerifyCode().then(response => {
+        console.log('verifycode:', response);
+        this.codeimg = response.data.codeImg;
+        this.trueVerifycode = response.data.token;
+      })
+    },
+
+    forgetPassword() {
+      /*
+      忘记密码
+      */
+      console.log('忘记密码按钮被触发')
+      let that = this
+      this.$router.push({
+        path: '/forgetPassword', query: {
+          isCustomer: that.customerLogin
+        }
+      });
+      //关闭登录界面
+      this.$emit('closeLogin');
+    }
+
+  }
 }
 </script>
 
